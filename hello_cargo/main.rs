1d02e4517b4fafffd19c59f51a807f6c59bf81f9
Trevisan <leonardosilio@alunos.utfpr.edu.br>
2022-06-24 05:53:22 -0300

    Remove Cargo to use only rust compiler

---
+++ hello_cargo/main.rs
@@ -0,0 +1,13 @@
+use std::io;
+
+fn main() {
+    println!("Hello, world!");
+
+    let mut value = String::new();
+
+    io::stdin()
+        .read_line(&mut value)
+        .expect("Failed to read line");
+    
+    println!("Value: {}", value);
+}
--- hello_cargo/src/main.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    println!("Hello, world!");
-    let mut a = "Ola mundo";
-    println!("{}", a);
-}
